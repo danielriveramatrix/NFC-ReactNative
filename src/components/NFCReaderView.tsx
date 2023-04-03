f4d676051031f3f0c62e20ed326dfcab107d2a79
Daniel Antonio Rivera <danriver@BAI-MD20FP2X9K.local>
2023-04-03 12:35:42 -0300

    FT/NFC

--- src/components/NFCReaderView.tsx
+++ src/components/NFCReaderView.tsx
@@ -1,37 +1,28 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
-import {observer} from 'mobx-react';
+import {View, Text, Button, NativeModules} from 'react-native';
 import NFCReaderViewModel from '../models/NFCReaderViewModel';
-import { NativeModules } from 'react-native';
 
 interface NFCReaderViewProps {
   viewModel: NFCReaderViewModel;
 }
 
-// const NFCReaderView: React.FC<NFCReaderViewProps> = observer(({viewModel}) => {
-//   return (
-//     <View>
-//       <Text>Lectura de NFC</Text>
-//       <Button title="Leer NFC" onPress={() => viewModel.startNFCReading()} />
-//       {viewModel.nfcData !== '' && <Text>{viewModel.nfcData}</Text>}
-//     </View>
-//   );
-// });
 const {NFCReaderManagerModule} = NativeModules;
+
 const onPress = () => {
-    NFCReaderManagerModule.readTag()
-  };
+  NFCReaderManagerModule.readTag((response: any) => {
+    console.log('Got response', response);
+  });
+};
+
 const NewModuleButton = () => {
-  
-
   return (
-
     <View>
- <Text>Lectura de NFC</Text>
-       <Button title="Leer NFC" onPress={onPress} />
-     </View>
-   
+      <Text>Lectura de NFC</Text>
+      <Button title="Leer NFC" onPress={onPress} />
+    </View>
   );
 };
 
 export default NewModuleButton;
+
+//Got unknown argument class: GotNFCResponse
